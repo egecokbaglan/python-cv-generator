0b4d5e2af03beca9dc1ee95a349bf2fa89e1fc27
Furkan Ege Çokbağlan <c.furkanege@ogr.iuc.edu.tr>
2026-02-09 02:18:26 +0300

    Add age validation loop

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+print("-welcome to CV generator program-")
 
+
+isim=input("adınız nedir?")
+while True:
+  yas=int(input("yaşınız nedir"))
+  if 0<yas<1000:
+    break
+  else:
+    print("tekrar dene")
