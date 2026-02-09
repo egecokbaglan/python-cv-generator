8d1165f502cd04b3cdce563e2016a370b93510de
Furkan Ege Çokbağlan <c.furkanege@ogr.iuc.edu.tr>
2026-02-10 00:56:50 +0300

    Add CV generation and ignore output file

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ print("-welcome to CV generator program-")
 
 isim=input("adınız nedir?")
 while True:
-  yas=int(input("yaşınız nedir"))
+  yas=int(input("yaşınız nedir?"))
   if 0<yas<1000:
     break
   else:
     print("tekrar dene")
+eğitim=input("hangi üniversiteden mezunsunuz?")
+mail=input("mailinizi giriniz:")
+def cv_metni():
+  metin=f"""
+  ---//CV\\---
+  name:{isim}
+  age:{yas}
+  Email:{mail}
+  Education:{eğitim}
+  """
+  return metin
+içerik=cv_metni()
+print(içerik)
+with open("cv.txt","w") as filecv:
+  filecv.write(içerik)
+
+
+
